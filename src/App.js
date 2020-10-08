2875687be597c533bb7ae03f27bb626f1aea93d7
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-10-08 15:46:55 -0500

    change background to parallax

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import "./App.css";
+import "./App.scss";
 import "./coin.scss";
 import Coin from './Coin';
 
@@ -15,7 +15,6 @@ function App() {
       )
       .then(res => {
         setCoins(res.data);
-        console.log(res.data);
       })
       .catch(error => console.log(error));
   }, []);
@@ -29,32 +28,37 @@ function App() {
   );
 
   return (
-    <div className='coin-app'>
-      <div className='coin-search'>
-        <h1 className='coin-text'>Search a currency</h1>
-        <form>
-          <input
-            className='coin-input'
-            type='text'
-            onChange={handleChange}
-            placeholder='Search'
-          />
-        </form>
+    <div className="App-master-container">
+      <div className='coin-app'>
+        <div className='coin-search'>
+          <h1 className='coin-text'>Search a currency</h1>
+          <form>
+            <input
+              className='coin-input'
+              type='text'
+              onChange={handleChange}
+              placeholder='Search'
+            />
+          </form>
+        </div>
+        {filteredCoins.map(coin => {
+          return (
+            <div className="coin-master-container">
+              <Coin
+                key={coin.id}
+                name={coin.name}
+                price={coin.current_price}
+                symbol={coin.symbol}
+                marketcap={coin.total_volume}
+                volume={coin.market_cap}
+                image={coin.image}
+                priceChange={coin.price_change_percentage_24h}
+              />
+            </div>
+            
+          );
+        })}
       </div>
-      {filteredCoins.map(coin => {
-        return (
-          <Coin
-            key={coin.id}
-            name={coin.name}
-            price={coin.current_price}
-            symbol={coin.symbol}
-            marketcap={coin.total_volume}
-            volume={coin.market_cap}
-            image={coin.image}
-            priceChange={coin.price_change_percentage_24h}
-          />
-        );
-      })}
     </div>
   );
 }
