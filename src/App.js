a4b3c352b0a3894f6b26d957b056b74205a1d0a0
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-11-19 17:10:10 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import "./App.scss";
 import "./coin.scss";
-import Coin from './Coin';
+import Coin from './components/Coin';
 
 function App() {
   const [coins, setCoins] = useState([]);
@@ -11,7 +11,7 @@ function App() {
   useEffect(() => {
     axios
       .get(
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=9999999&page=1&sparkline=false'
       )
       .then(res => {
         setCoins(res.data);
@@ -29,18 +29,18 @@ function App() {
 
   return (
     <div className="App-master-container">
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a currency</h1>
+        <form>
+          <input
+            className='coin-input'
+            type='text'
+            onChange={handleChange}
+            placeholder='Search'
+          />
+        </form>
+      </div>
       <div className='coin-app'>
-        <div className='coin-search'>
-          <h1 className='coin-text'>Search a currency</h1>
-          <form>
-            <input
-              className='coin-input'
-              type='text'
-              onChange={handleChange}
-              placeholder='Search'
-            />
-          </form>
-        </div>
         {filteredCoins.map(coin => {
           return (
             <div className="coin-master-container">
